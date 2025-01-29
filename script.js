e049302756768b2dc27028abb0e3093d0c078ee9
seohyeon-lee-2025 <seohyeonlee2020@gmail.com>
2025-01-29 23:58:39 +0900

    feat: removed dropdown and added separate input for words and chars

--- script.js
+++ script.js
@@ -1,54 +1,65 @@
 const textInput = document.getElementById("text-input")
 const charCount = document.getElementById("char-count")
+const wordCount = document.getElementById("word-count")
+
 const charLimit = document.getElementById("char-limit")
+const wordLimit = document.getElementById("word-limit")
 
-/*
-const setMaxChars = () => {
-	const maxChars = parseInt(charLimit.value)
-	charCount.textContent = `Character Count: 0/${maxChars}`
-	textInput.disabled = false;
-	return maxChars
-}
-*/
+
 let maxChars = 0
-let textLength = 0
-charCount.textContent = ``
+let maxWords = 0
 
-/*takes input from maxChars*/
-charLimit.addEventListener("input", () => {
-	maxChars = parseInt(charLimit.value)
-	if (textLength){
-		charCount.textContent = `${textLength}/${maxChars} Characters`
-	}
-	else{
-		charCount.textContent = ` /${maxChars} Characters`
-	}
-})
+let numChars = 0
+let numWords = 0
+
+charCount.textContent = `0 Characters`
+wordCount.textContent = `0 Words`
+
 
 const counter = () => {
+	numChars = textInput.value.length
+	numWords = textInput.value.trim().split(/\s+/).filter(word => word.length > 0).length;
 
-	textLength = textInput.value.length
+	charCount.textContent = `${numChars} Characters`
+	wordCount.textContent = `${numWords} Words`
 
-/* if user inputs charLimit */
-if (maxChars){
+}
+
+const setLimits = () => {
+	/* if user inputs charLimit */
+if (charLimit.value){
+	numChars = textInput.value.length
+	maxChars = parseInt(charLimit.value)
 	textInput.setAttribute("maxlength", maxChars)
-	charCount.textContent = `${textLength}/${maxChars} Characters`
-	if (textLength > maxChars){
-		textInput.value = textInput.value.slice(0, maxChars)
-	  }
+	charCount.textContent = numChars == 1 ? `${numChars}/${maxChars} Characters` : `${numChars}/${maxChars} Characters`
+	checkMax(numChars, maxChars, charCount)
+}
 
-	if (textLength === maxChars){
-		charCount.classList.add("red")
+if (wordLimit.value){
+	numWords = textInput.value.trim().split(/\s+/).filter(word => word.length > 0).length;
+	maxWords = parseInt(wordLimit.value)
+	wordCount.textContent = numWords == 1 ? `${numWords}/${maxWords} Word` : `${numWords}/${maxWords} Words`
+}
+}
+
+const checkMax = (currentCount, maxCount, displayCount) => {
+	/*if (currentCount > maxCount){
+		textInput.value = textInput.value.slice(0, maxCount)
+	  } */
+	if (currentCount >= maxCount){
+		displayCount.classList.add("red")
 		}
 	else{
-		charCount.classList.remove("red")
+		displayCount.classList.remove("red")
 	}
 }
-else{
-	charCount.textContent = `${textLength} Characters`
-	}
 
-}
+charLimit.addEventListener("input", setLimits)
+/*charLimit.addEventListener("change", () => {checkMax(numChars, maxChars, charCount)}) */
+
+wordLimit.addEventListener("input", setLimits)
+/*wordLimit.addEventListener("change", () => {checkMax(numWords, maxWords, wordCount)})*/
 
 textInput.addEventListener("input", counter)
+textInput.addEventListener("input", setLimits)
 
