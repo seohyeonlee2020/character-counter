5745e1c84230a2c19ab0465c89910129404aee3c
seohyeon-lee-2025 <seohyeonlee2020@gmail.com>
2025-01-29 20:35:50 +0900

    feat: added dropdown

--- script.js
+++ script.js
@@ -1,22 +1,54 @@
 const textInput = document.getElementById("text-input")
-
 const charCount = document.getElementById("char-count")
+const charLimit = document.getElementById("char-limit")
+
+/*
+const setMaxChars = () => {
+	const maxChars = parseInt(charLimit.value)
+	charCount.textContent = `Character Count: 0/${maxChars}`
+	textInput.disabled = false;
+	return maxChars
+}
+*/
+let maxChars = 0
+let textLength = 0
+charCount.textContent = ``
+
+/*takes input from maxChars*/
+charLimit.addEventListener("input", () => {
+	maxChars = parseInt(charLimit.value)
+	if (textLength){
+		charCount.textContent = `${textLength}/${maxChars} Characters`
+	}
+	else{
+		charCount.textContent = ` /${maxChars} Characters`
+	}
+})
 
 const counter = () => {
-const textLength = textInput.value.split("").length
 
-if (textLength >= 50){
-  textInput.textContent = textInput.value.slice(50)
+	textLength = textInput.value.length
+
+/* if user inputs charLimit */
+if (maxChars){
+	textInput.setAttribute("maxlength", maxChars)
+	charCount.textContent = `${textLength}/${maxChars} Characters`
+	if (textLength > maxChars){
+		textInput.value = textInput.value.slice(0, maxChars)
+	  }
+
+	if (textLength === maxChars){
+		charCount.classList.add("red")
+		}
+	else{
+		charCount.classList.remove("red")
+	}
 }
-  charCount.textContent = `Character Count: ${textLength}/50`
-
-if (textLength === 50){
-  charCount.classList.add("red")
-  }
 else{
-  charCount.classList.remove("red")
-  }
+	charCount.textContent = `${textLength} Characters`
+	}
 
 }
 
-textInput.addEventListener("keyup", counter)
+textInput.addEventListener("input", counter)
+
